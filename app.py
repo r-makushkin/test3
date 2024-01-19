7a25591e1a69a53e19fa95e4a301a99c921de559
Aleksei Abramov <aleksey-ab99a@mail.ru>
2024-01-19 20:11:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from st_pages import Page, show_pages
 
-show_pages([
+show_pages(
+    [
         Page('app.py', 'Главная'),
         Page('pages/user_input_page.py', 'Ввод пользовательской информации'),
         Page('pages/analitycs_page.py', 'Аналитика'),
         Page('pages/results.py', 'Результаты')
-])
+    ]
+)
+
