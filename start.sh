baf8ab5e2dcc390d6c0944cc44786d1cc655e48d
Aleksei Abramov <aleksey-ab99a@mail.ru>
2024-01-19 19:27:48 +0300

    02v

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo apt update
+apt-get update && apt-cache search python3.10
+sudo apt update pip
+pip install streamlit telethone asyncio pandas torch transformers
+cd models
+mkdir "local_tokenizer"
+python3 get_models.py
